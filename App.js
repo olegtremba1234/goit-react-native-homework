11b960732a26a9f5c4462f972a5c2da5ba6a2793
olegtremba1234 <olegtremba@gmail.com>
2023-03-16 11:43:01 +0200

    add hw1

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!🎉</Text>
+      <Text>This is strong middle React Native Developer! 👨‍💻 </Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
